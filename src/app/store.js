2f6e5dc01a23e138ba54f0b9b424ad429c9d0c58
Zaur <zaurik87@gmail.com>
2026-02-21 10:09:22 +0400

    add postReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import themeReducer from "../features/theme/themeSlice";
+import postsReducer from "../features/posts/postSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     theme: themeReducer,
+    posts: postsReducer,
   },
 });
-
-
