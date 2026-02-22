edc1613b4ea59c22d2ad91d9f53db23e28bae7d2
Zaur <zaurik87@gmail.com>
2026-02-22 09:58:12 +0400

    add ToastContainer to AppLayout

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import ToastContainer from "../ToastContainer";
 
 export default function AppLayout() {
   return (
     <div className="min-h-screen">
       <Navbar />
+      <ToastContainer />
       <main className="max-w-5xl px-4 py-6 mx-auto">
         <Outlet />
       </main>
