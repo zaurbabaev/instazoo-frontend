a9f56bdc8508774b6a0a086e4e6847890fded3f1
Zaur <zaurik87@gmail.com>
2026-02-22 09:55:37 +0400

    add toastReducer to store.js

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import themeReducer from "../features/theme/themeSlice";
 import postsReducer from "../features/posts/postsSlice";
-
+import toastReducer from "../features/toast/toastSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     theme: themeReducer,
     posts: postsReducer,
+    toast: toastReducer,
   },
 });
